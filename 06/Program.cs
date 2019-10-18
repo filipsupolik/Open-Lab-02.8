92374ea23c037706501d723dc7838c3d87f879b6
filip šupolík <filipsupolik25@gmail.com>
2019-10-18 08:52:46 +0200

    Completed 02.8

---
+++ 06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06
+{
+    class Program
+    {
+        public static bool IsEmpty(string str)
+        {
+           
+            if (str == "")
+            {
+                return (true);
+            }
+            else
+            {
+                return false;
+            }
+
+                
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
